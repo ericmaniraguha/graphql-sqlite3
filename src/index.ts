d94932982bdd74588fc3ce1eba6073cb7ba74737
Eric Maniraguha <ericmaniraguha@gmail.com>
2022-06-15 23:25:44 +0200

    setting up gql with sqlite3

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
+import { ApolloServer } from 'apollo-server';
+import { BookResolver } from './resolvers/BookResolver'; // add this
+import { buildSchema } from 'type-graphql';
+
+async function main() {
+  const connection = await createConnection();
+  const schema = await buildSchema({
+    resolvers: [BookResolver], // add this
+  });
+  const server = new ApolloServer({ schema });
+  await server.listen(4000);
+  console.log('Server has started!');
+}
+main();
